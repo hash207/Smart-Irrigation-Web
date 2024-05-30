1da439fee9ebef4a0feb6e0777085129bbcd9e75
Hashem Alsharif <hashemalshar20.7@gmail.com>
2024-05-30 20:33:47 +0300

    Create the Android App

--- Android app/main.py
+++ Android app/main.py
@@ -1,8 +1,19 @@
+from requests import get, ConnectTimeout
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivymd.uix.screen import MDScreen
 
-class Home(MDScreen):...
+main_server = "http://192.168.0.111:5000"
+
+class Home(MDScreen):
+    def toggel_led(self, btn):
+        ip = self.ids.ip.text
+        try:
+            rout = btn.text.replace(" ", "").upper()
+            get(f"{main_server}/room1/{rout}")
+            #self.ids.resp.text = url
+        except ConnectTimeout:
+            self.ids.resp.text = "Connection Timeout"
 
 class main_app(MDApp):
     def build(self):
@@ -12,4 +23,3 @@ class main_app(MDApp):
 
 if __name__ == "__main__":
     main_app().run()
-
