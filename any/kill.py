279919fe4dcc9b4c0f0e3b792d07ccd0b1e76200
hashemalshar207 <33919400-hashemalshar207@users.noreply.replit.com>
2025-02-13 15:18:20 +0000

    User checkpoint: Update kill script to only print PID, and configure port forwarding in .replit.

--- any/kill.py
+++ any/kill.py
@@ -3,5 +3,5 @@ import os
 output = os.popen("netstat -lpnt | grep :5000").read()
 
 data = output[output.index("N")+7:output.index("/")]
-#print(data)
-os.popen(f"kill {data}")
+print(data)
+#os.popen(f"kill {data}")
--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
-source smart_venv/bin/activate
-nohup python3 "Web app/main.py" &  ngrok http --url=starfish-regular-lightly.ngrok-free.app 5000
+nohup python "Web app/main.py" &  ngrok http --url=starfish-regular-lightly.ngrok-free.app 5000
 python3 any/kill.py
