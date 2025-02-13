21625ae9351b4fe3916b1a821c30ba9273f1b0cd
hashemalshar207 <33919400-hashemalshar207@users.noreply.replit.com>
2025-02-13 15:32:01 +0000

    User checkpoint: Fix: Configure ngrok to use a dedicated directory and address authentication error.

--- start_ngrok.py
+++ start_ngrok.py
@@ -1,11 +1,11 @@
-
 from pyngrok import ngrok, conf
 import os
 
 # Configure ngrok to use a local directory
 home_dir = os.path.expanduser("~")
-ngrok_path = os.path.join(home_dir, ".ngrok")
-conf.get_default().ngrok_path = ngrok_path
+ngrok_path = os.path.join(home_dir, "ngrok_bin")
+os.makedirs(ngrok_path, exist_ok=True)
+conf.get_default().ngrok_path = os.path.join(ngrok_path, "ngrok")
 
 # Start ngrok tunnel
 url = ngrok.connect(5000)
