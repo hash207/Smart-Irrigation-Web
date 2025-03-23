b1e62bb13415ca8699e5611445bf782fac509ba9
hash207 <hashemalshar20.7@gmail.com>
2025-03-23 20:04:36 +0300

    Refactor kill.py to improve termination process

--- any/kill.py
+++ any/kill.py
@@ -6,5 +6,6 @@ output = os.popen("netstat -lpnt | grep :5000").read()
 try:
     data = ''.join(re.findall('\d', output[output.index("N")+1:]))
     os.popen(f"kill {data}")
+    print("operation termenated succesfully")
 except ValueError:
-    pass
+    print("Operation did not termenat succeafully")
