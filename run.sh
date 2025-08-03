b2a570a5a68bf29cdd5acaebfb86adb0050cc3d1
hash207 <hashemalshar20.7@gmail.com>
2025-08-03 22:14:31 +0300

    Remove unnecessary direction command

--- run.sh
+++ run.sh
@@ -1,18 +1,14 @@
-
-#!/bin/bash
-cd Desktop/Hashem/Python/Smart-Home-Project
-
 # Activate the virtual environment
 source smart_venv/bin/activate
 
 # Start the Flask app in the background
-python "Web app/main.py" &
+python3 "Web app/main.py" &
 
 # Wait a few seconds for Flask to start
 sleep 3
 
 # Start ngrok tunnel
-python any/start_ngrok.py
+python3 any/start_ngrok.py
 
 # End working in background
-python any/kill.py
+python3 any/kill.py
